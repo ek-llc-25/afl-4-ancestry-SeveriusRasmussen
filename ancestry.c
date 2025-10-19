a404baaf42c71ac4c7a453633637f50d0b18a6ad
SeveriusRasmussen <sora0001@stud.kea.dk>
2025-10-19 12:10:34 +0200

    Færdig med kode. Medmindre LargeTree skal også gøres.

--- ancestry.c
+++ ancestry.c
@@ -6,42 +6,107 @@
 #include "ancestry.h"
 
 struct person_t *make_person(char *first_name, char *last_name, int year_born) {
-    // TODO: Allokér plads til en `struct person_t`.
-    // TODO: Allokér også plads til kopier af `first_name` og `last_name`.
-    // TODO: Kopiér indholdet fra `first_name` og `last_name` over i kopierne.
-    // TODO: Husk at gøre plads til både alle tegnene i navnene + 1 NUL-byte.
+    // Allokér plads i heap'en til en `struct person_t`.
+    struct person_t *new = malloc(sizeof(struct person_t));
+    if (new == NULL) return NULL;
+    //DONE (new->...) Allokér også plads i heap'en til kopier af `first_name` og `last_name`.
+    //DONE (strcpy();) Kopiér indholdet fra `first_name` og `last_name` over i kopierne.
+    //DONE (strlen(last_name) + 1) Husk at gøre plads til både alle tegnene i navnene + 1 NUL-byte.
 
-    return NULL;
+    // First name
+    new->first_name = malloc(strlen(first_name) + 1);
+    if (new->first_name == NULL)
+    {
+        free(new);
+        return NULL;
+    }
+    strcpy(new->first_name, first_name);
+
+    // Last name
+    new->last_name = malloc(strlen(last_name) + 1);
+    if (new->last_name == NULL)
+    {
+        free(new->first_name); //free first_name hvis last_name fejler.
+        free(new);
+        return NULL;
+    }
+    strcpy(new->last_name, last_name);
+
+    // Year
+    new->year_born = year_born;
+    return new;
 }
 
 void free_person(struct person_t *person) {
-    // TODO: Der skal være et free()-kald for hver malloc() i make_person().
-    // TODO: Du må gerne sætte `first_name` og `last_name` til NULL *EFTER* de er free()'et.
+    //Done Der skal være et free()-kald for hver malloc() i make_person().
+    //Done Du må gerne sætte `first_name` og `last_name` til NULL *EFTER* de er free()'et.
+    //First Name
+    free(person->first_name);
+    person->first_name = NULL;
+
+    //Last Name
+    free(person->last_name);
+    person->last_name = NULL;
+
+    free(person);
+    person = NULL;
+
 }
 
 struct ancestry_node_t *make_tree(struct person_t *person) {
-    // TODO: Allokér plads til en `struct ancestry_node_t`.
-    // TODO: Sæt `person` til input-personen.
-    // TODO: Sæt `mom` og `dad` til NULL.
-    return NULL;
+    //DONE Allokér plads til en `struct ancestry_node_t`.
+    struct ancestry_node_t *node = malloc(sizeof(struct ancestry_node_t));
+    if (node == NULL) return NULL;
+
+    //DONE Sæt `person` til input-personen.
+    node->person = person;
+
+    //DONE Sæt `mom` og `dad` til NULL.
+    node->mom = NULL;
+    node->dad = NULL;
+
+    return node;
 }
 
 bool add_mom(struct ancestry_node_t *child_node, struct person_t *mom_person) {
-    // TODO: Hvis der allerede er en mor-stamtavle, skal funktionen fejle.
-    // TODO: Hvis ikke, skal `mom_person` indsættes i en `mom_node` som først skal allokeres.
+    //Done Hvis der allerede er en mor-stamtavle, skal funktionen fejle.
+    if (!child_node || !mom_person) return false; // Hvis ingen child eller mom, fejler det.
+    if (child_node->mom) return false; // Hvis child har allerede en mom, fejler det.
+
+    //Done Hvis ikke, skal `mom_person` indsættes i en `mom_node` som først skal allokeres.
+    struct ancestry_node_t *mom_node = make_tree(mom_person);
+    if (!mom_node) return false;
+
+    child_node->mom = mom_node;
     return false;
 }
 
 bool add_dad(struct ancestry_node_t *child_node, struct person_t *dad_person) {
-    // TODO: Hvis der allerede er en far-stamtavle, skal funktionen fejle.
-    // TODO: Hvis ikke, skal `dad_person` indsættes i en `dad_node` som først skal allokeres.
+    //Done Hvis der allerede er en far-stamtavle, skal funktionen fejle.
+    if (!child_node || !dad_person) return false;
+    if (child_node->dad) return false;
+
+    //Done Hvis ikke, skal `dad_person` indsættes i en `dad_node` som først skal allokeres.
+    struct ancestry_node_t *dad_node = make_tree(dad_person);
+    if (!dad_node) return false;
+
+    child_node->dad = dad_node;
     return false;
 }
 
 void free_tree(struct ancestry_node_t *node) {
-    // TODO: Kald `free_person()` på personen i noden.
-    // TODO: Kald `free_tree()` på begge forældre-stamtræer.
-    // TODO: Sæt pointerne til NULL efter de er free()'et.
+    if (!node) return;
+    //Done Kald `free_person()` på personen i noden.
+    free_tree(node->person);
+    //Done Kald `free_tree()` på begge forældre-stamtræer.
+    free_tree(node->mom);
+    free_tree(node->dad);
+    //Done Sæt pointerne til NULL efter de er free()'et.
+    node->person = NULL;
+    node->mom = NULL;
+    node->dad = NULL;
+
+    free(node);
 }
 
 void print_tree_recursive(struct ancestry_node_t *node, char *prefix, bool is_last) {
